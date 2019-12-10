38ae753281bb0d167d878b04d72c55ae2782dba8
Deana <deana.pennell@gmail.com>
2019-12-09 19:47:29 -0500

    1b

---
+++ 1/1b.py
@@ -0,0 +1,22 @@
+import sys
+
+def get_fuel( mass ):
+    """crappy recursive function to calculate amonut of fuel needed"""
+    print mass
+    if( mass // 3 <= 2 ):
+        return 0 
+    else:
+        temp = (mass // 3) - 2
+        return temp + get_fuel( (mass // 3) - 2 )
+
+filename = sys.argv[1]
+data = open(filename,"r")
+
+total = 0
+for line in data:
+    var = get_fuel(int(line))
+    print var
+    print 'next'
+    total = total + var
+print("the total fuel required is {}".format(total))
+
