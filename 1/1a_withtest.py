2929fd1879ef25aec3bd91d98ffb8898e7695b9a
Deana <deana.pennell@gmail.com>
2020-03-01 23:48:50 -0500

    now with total test

--- 1/1a_withtest.py
+++ 1/1a_withtest.py
@@ -3,6 +3,12 @@ import pytest
 def getfuel( mass ):
    return (mass // 3) - 2
 
+def total( masses ):
+    total = 0
+    for mass in masses:
+       total = total + getfuel( mass )
+    return total
+
 def test_mass12yield2():
     returnvalue = getfuel(12)
     assert returnvalue == 2
@@ -18,3 +24,8 @@ def test_mass1969yield654():
 def test_mass100756yield33583():
     returnvalue = getfuel(100756)
     assert returnvalue == 33583
+
+def test_sumis34241():
+    testmasses = [12,14,1969,100756]
+    returnvalue = total( testmasses )
+    assert returnvalue == 34241
