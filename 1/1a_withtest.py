66d58d4a4765e244b80069c3510bf4f546ce5b62
Deana <deana.pennell@gmail.com>
2020-03-04 22:29:25 -0500

    reading input from file passed on command line... no tests

--- 1/1a_withtest.py
+++ 1/1a_withtest.py
@@ -1,27 +1,47 @@
 import pytest
+import sys
+import argparse
 
 def getfuel( mass ):
    return (mass // 3) - 2
 
-def total( masses ):
+def totalfuel( masses ):
     total = 0
     for mass in masses:
        total = total + getfuel( mass )
     return total
 
-def test_mass12yield2():
+def getargs():
+   parser = argparse.ArgumentParser();
+   parser.add_argument("filename")
+   args = parser.parse_args();
+   return args
+
+def getdata( args ):
+   filename = args.filename
+   data = open(filename,"r")
+   l = []
+   for line in data:
+       l.append( int(line) )
+   return l
+
+data = getdata( getargs() )
+print("the total fuel required is {}".format( totalfuel( data ) ) )
+
+
+def test_mass12():
     returnvalue = getfuel(12)
     assert returnvalue == 2
 
-def test_mass14yield2():
+def test_mass14():
     returnvalue = getfuel(14)
     assert returnvalue == 2
 
-def test_mass1969yield654():
+def test_mass1969():
     returnvalue = getfuel(1969)
     assert returnvalue == 654
 
-def test_mass100756yield33583():
+def test_mass100756():
     returnvalue = getfuel(100756)
     assert returnvalue == 33583
 
